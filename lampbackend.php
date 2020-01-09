627d1c634cbc9202ca44ca83a2327ba92e9621d6
keatontang <52551143+keatontang@users.noreply.github.com>
2020-01-10 11:02:21 +1300

    Update lampbackend.php

--- lampbackend.php
+++ lampbackend.php
@@ -66,6 +66,7 @@ print <<<EOF
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <head>
 <body>
+<link rel="shortcut icon" href="favicon.ico" />
 <title>PigeonPost</title>
 <style>
 body{
